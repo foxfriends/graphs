e0e6da3fbb771e4565c93b2346a2be12229c4053
Cameron Eldridge <cameldridge@gmail.com>
2022-10-07 23:10:07 -0400

    fix some bugs

--- app/routes/api/github_pull_request_reviewers.ts
+++ app/routes/api/github_pull_request_reviewers.ts
@@ -2,57 +2,61 @@ import { prop } from "ramda";
 import { connect } from "~/lib/database.ts";
 
 export const GET = async (request) => {
-  const { searchParams } = new URL(request.url);
   const db = await connect();
-  const {
-    rows: [repository],
-  } = await db.queryObject`
+  try {
+    const { searchParams } = new URL(request.url);
+    const {
+      rows: [repository],
+    } = await db.queryObject`
     SELECT owner, name
       FROM github_repositories
       WHERE name ILIKE ${searchParams.get("name")}
         AND owner ILIKE ${searchParams.get("owner")}
   `;
-  if (!repository) {
-    return Response.json({ error: "Not Found" }, { status: 404 });
-  }
-  const { owner, name } = repository;
-  console.log(`Loading pull requests for ${owner}/${name}`);
-  const { rows: pullRequests } = await db.queryObject`
+    if (!repository) {
+      return Response.json({ error: "Not Found" }, { status: 404 });
+    }
+    const { owner, name } = repository;
+    console.log(`Loading pull requests for ${owner}/${name}`);
+    const { rows: pullRequests } = await db.queryObject`
     SELECT id, title, author
       FROM github_pull_requests
       WHERE repository_owner = ${owner}
         AND repository_name = ${name}
   `;
-  console.log(`${pullRequests.length} pull requests found`);
-  const { rows: reviewers } = await db.queryObject`
+    console.log(`${pullRequests.length} pull requests found`);
+    const { rows: reviewers } = await db.queryObject`
     SELECT pull_request_id as "pullRequestId", reviewer
       FROM github_pull_request_reviewers
       WHERE repository_owner = ${owner}
         AND repository_name = ${name}
   `;
-  const { rows: suggestedReviewers } = await db.queryObject`
+    const { rows: suggestedReviewers } = await db.queryObject`
     SELECT pull_request_id as "pullRequestId", reviewer
       FROM github_pull_request_suggested_reviewers
       WHERE repository_owner = ${owner}
         AND repository_name = ${name}
   `;
-  const logins = Array.from(
-    new Set([
-      ...pullRequests.map(prop("author")),
-      ...reviewers.map(prop("reviewer")),
-    ])
-  );
-  const { rows: users } = await db.queryObject`
+    const logins = Array.from(
+      new Set([
+        ...pullRequests.map(prop("author")),
+        ...reviewers.map(prop("reviewer")),
+      ])
+    );
+    const { rows: users } = await db.queryObject`
     SELECT login, avatar_url as "avatarUrl"
       FROM github_users
       WHERE login = ANY(${logins})
   `;
-  console.log(`${users.length} pull requests found`);
+    console.log(`${users.length} pull requests found`);
 
-  return Response.json({
-    users,
-    reviewers,
-    suggestedReviewers,
-    pullRequests,
-  });
+    return Response.json({
+      users,
+      reviewers,
+      suggestedReviewers,
+      pullRequests,
+    });
+  } finally {
+    await db.release();
+  }
 };
--- app/routes/api/github_repositories.ts
+++ app/routes/api/github_repositories.ts
@@ -2,7 +2,11 @@ import { connect } from "~/lib/database.ts";
 
 export const GET = async (request) => {
   const db = await connect();
-  const { rows: repositories } =
-    await db.queryObject`SELECT owner, name FROM github_repositories ORDER BY owner ASC, name ASC`;
-  return Response.json({ repositories });
+  try {
+    const { rows: repositories } =
+      await db.queryObject`SELECT owner, name FROM github_repositories ORDER BY owner ASC, name ASC`;
+    return Response.json({ repositories });
+  } finally {
+    await db.release();
+  }
 };
