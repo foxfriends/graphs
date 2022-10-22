ea6cc17033925403afb04c4bff454d2113ce2580
Cameron Eldridge <cameldridge@gmail.com>
2022-10-22 14:47:25 -0400

    allow passing multiple repositories at once

--- populate/commands/gh_prs.ts
+++ populate/commands/gh_prs.ts
@@ -1,4 +1,5 @@
 import { type MapTypes } from "cliffy";
+import { split } from "ramda";
 import { transaction } from "$lib/database/transaction.ts";
 import { savePullRequest } from "$lib/database/github_pull_request.ts";
 import {
@@ -15,57 +16,60 @@ import logger from "$lib/logger.ts";
 
 export async function ghPrs(
   _options: MapTypes<void>,
-  repositoryOwner: string,
-  repositoryName: string,
+  repositories: string[],
 ) {
-  const { name, owner } = await getRepository({
-    owner: repositoryOwner,
-    name: repositoryName,
-  });
-  logger.info(`Analyzing GitHub repository: ${owner.login}/${name}`);
-  const repository: GithubRepository = {
-    owner: owner.login,
-    name,
-  };
-  const pullRequests = await getPullRequests(repository);
-  logger.info(`${pullRequests.length} pull requests found`);
-  const logins = new Set(pullRequests.flatMap(associatedUsers));
-  const users = await Promise.all(Array.from(logins, getUser));
-  logger.info(`${users.length} users found`);
-
-  await transaction(async function saveGithubPullRequests(db) {
-    for (const user of [owner, ...users]) await saveUser(db, user);
-    await saveRepository(db, repository);
-    for (const pullRequest of pullRequests) {
-      await savePullRequest(db, {
-        ...pullRequest,
-        repositoryOwner: repository.owner,
-        repositoryName: repository.name,
+  return Promise.all(
+    repositories.map(split("/")).map(async ([repositoryOwner, repositoryName]) => {
+      const { name, owner } = await getRepository({
+        owner: repositoryOwner,
+        name: repositoryName,
       });
-      for (const reviewer of pullRequest.requestedReviewers) {
-        await savePullRequestReviewer(db, {
-          pullRequestId: pullRequest.id,
-          repositoryOwner: repository.owner,
-          repositoryName: repository.name,
-          reviewer,
-        });
-      }
-      for (const review of pullRequest.reviews) {
-        await savePullRequestReview(db, {
-          pullRequestId: pullRequest.id,
-          repositoryOwner: repository.owner,
-          repositoryName: repository.name,
-          ...review,
-        });
-      }
-      for (const reviewer of pullRequest.suggestedReviewers) {
-        await savePullRequestSuggestedReviewer(db, {
-          pullRequestId: pullRequest.id,
-          repositoryOwner: repository.owner,
-          repositoryName: repository.name,
-          reviewer,
-        });
-      }
-    }
-  });
+      logger.info(`Analyzing GitHub repository: ${owner.login}/${name}`);
+      const repository: GithubRepository = {
+        owner: owner.login,
+        name,
+      };
+      const pullRequests = await getPullRequests(repository);
+      logger.info(`${pullRequests.length} pull requests found`);
+      const logins = new Set(pullRequests.flatMap(associatedUsers));
+      const users = await Promise.all(Array.from(logins, getUser));
+      logger.info(`${users.length} users found`);
+
+      await transaction(async function saveGithubPullRequests(db) {
+        for (const user of [owner, ...users]) await saveUser(db, user);
+        await saveRepository(db, repository);
+        for (const pullRequest of pullRequests) {
+          await savePullRequest(db, {
+            ...pullRequest,
+            repositoryOwner: repository.owner,
+            repositoryName: repository.name,
+          });
+          for (const reviewer of pullRequest.requestedReviewers) {
+            await savePullRequestReviewer(db, {
+              pullRequestId: pullRequest.id,
+              repositoryOwner: repository.owner,
+              repositoryName: repository.name,
+              reviewer,
+            });
+          }
+          for (const review of pullRequest.reviews) {
+            await savePullRequestReview(db, {
+              pullRequestId: pullRequest.id,
+              repositoryOwner: repository.owner,
+              repositoryName: repository.name,
+              ...review,
+            });
+          }
+          for (const reviewer of pullRequest.suggestedReviewers) {
+            await savePullRequestSuggestedReviewer(db, {
+              pullRequestId: pullRequest.id,
+              repositoryOwner: repository.owner,
+              repositoryName: repository.name,
+              reviewer,
+            });
+          }
+        }
+      });
+    }),
+  );
 }
